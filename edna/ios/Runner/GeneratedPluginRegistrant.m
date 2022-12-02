3cd7cd3988a65bbd9785f571d01046e2c36ae35d
Julian Fliegler <julianfliegler@gmail.com>
2022-12-02 14:32:30 -0800

    add camera plugin

--- edna/ios/Runner/GeneratedPluginRegistrant.m
+++ edna/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,9 +6,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<camera_avfoundation/CameraPlugin.h>)
+#import <camera_avfoundation/CameraPlugin.h>
+#else
+@import camera_avfoundation;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
 }
 
 @end
