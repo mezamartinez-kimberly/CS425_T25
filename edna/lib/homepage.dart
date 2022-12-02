e62cc2d1b91e5faedc3245cdab446df6c890b889
watson-john <watson221@nevada.unr.edu>
2022-12-02 14:38:21 -0800

    Nav Bar with colors

--- edna/lib/homepage.dart
+++ edna/lib/homepage.dart
@@ -11,33 +11,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       bottomNavigationBar: GNav(
         haptic: true, // haptic feedback
         gap: 8,
         iconSize: 24,
-        padding: EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
             horizontal: 5, vertical: 20), // navigation bar padding
         tabs: [
           GButton(
             icon: Icons.camera_alt_rounded,
             text: 'Camera',
+            iconActiveColor: Colors.lightBlue,
+            backgroundColor: Colors.lightBlue.withOpacity(0.2),
           ),
           GButton(
             icon: Icons.shopping_basket,
             text: 'Pantry',
+            iconActiveColor: Colors.red,
+            backgroundColor: Colors.red.withOpacity(0.2),
           ),
           GButton(
             icon: Icons.calendar_today_rounded,
-            text: 'Add',
+            text: 'Calendar',
+            iconActiveColor: Colors.orange,
+            backgroundColor: Colors.orange.withOpacity(0.2),
           ),
           GButton(
             icon: Icons.list_alt,
             text: 'Stats',
+            iconActiveColor: Colors.purple,
+            backgroundColor: Colors.purple.withOpacity(0.2),
           ),
           GButton(
             icon: Icons.person,
             text: 'Profile',
+            iconActiveColor: Colors.grey,
+            backgroundColor: Colors.grey.withOpacity(0.2),
           ),
         ],
       ),
