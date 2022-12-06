0710a7be85ec647d0341b86273d578ef627e27a9
Watson-John <70344341+Watson-John@users.noreply.github.com>
2022-12-06 10:50:52 -0800

    Added Scraping code

---
+++ StillTasty.py
@@ -0,0 +1,7 @@
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import re
+
+
---
+++ edna/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/johnwatson/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/johnwatson/Library/CloudStorage/GoogleDrive-john.watson3091@gmail.com/Other computers/My Computer/Current School/Flutter/CS425_T25/edna"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
