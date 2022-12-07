95afcf5c1ad606f3f372408fd7c3dbce23fd55f9
Watson-John <70344341+Watson-John@users.noreply.github.com>
2022-12-06 21:44:20 -0800

    Rebase with Git Ignore

--- edna/ios/Runner/GeneratedPluginRegistrant.m
+++ edna/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,16 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<camera_avfoundation/CameraPlugin.h>)
-#import <camera_avfoundation/CameraPlugin.h>
-#else
-@import camera_avfoundation;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
 }
 
 @end
