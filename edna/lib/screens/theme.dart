009050099aba38fa699bacbb654aa68bd3429c4c
Cade Hockersmith <invent4u369@gmail.com>
2023-02-14 09:12:40 -0800

    Theme Framework
    Going to convert existing styles and widgets to new theme format rather than having each page handle its own styles.

--- edna/lib/screens/theme.dart
+++ edna/lib/screens/theme.dart
@@ -1,26 +1,35 @@
 /*
 ==============================
 *    Title: theme.dart
-*    Author: Julian Fliegler
-*    Date: Dec 2022
+*    Author: Cade Hockersmith
+*    Date: Feb 2022
 ==============================
 */
 
 /* References: 
-https://medium.com/@nickysong/creating-a-custom-color-swatch-in-flutter-554bcdcb27f3
+https://docs.flutter.dev/cookbook/design/themes
+https://medium.com/flutter-community/themes-in-flutter-part-1-75f52f2334ea
+https://www.flutterbeads.com/change-theme-text-color-in-flutter/
+
 */
 
+//Imports
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class MyTheme extends StatelessWidget {
-  const MyTheme({super.key});
+class ThemeConstants {
+  static final ThemeData lightTheme = ThemeData(
+    //Initial Theme Setup
+    brightness: Brightness.light,
 
-  // function that allows user to create own material color type
-  MaterialColor createMaterialColor(Color color) {
-    List strengths = <double>[.05];
-    Map<int, Color> swatch = {};
-    final int r = color.red, g = color.green, b = color.blue;
+    //Universal Styles - Text
+      //fontFamily: 'Raleway', //Esablishing the font family used throughout the project.
+      
+      displayLarge: TextStyle(color: Colors.deepPurpleAccent,), //Largest text, for most significant element on the page.
+      displayMedium: TextStyle(color: Colors.deepPurpleAccent),
+      displaySmall: TextStyle(color: Colors.deepPurpleAccent),
 
+<<<<<<< Updated upstream
     for (int i = 1; i < 10; i++) {
       strengths.add(0.1 * i);
     }
@@ -35,9 +44,107 @@ class MyTheme extends StatelessWidget {
     }
     return MaterialColor(color.value, swatch);
   }
+=======
+      headlineLarge: TextStyle(color: Colors.deepPurpleAccent), //Short, high-emphasis text
+      headlineMedium: TextStyle(color: Colors.deepPurpleAccent),
+      headlineSmall: TextStyle(color: Colors.deepPurpleAccent),
+>>>>>>> Stashed changes
 
-  @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
+      bodyLarge: TextStyle(color: Colors.deepPurpleAccent), //Long blocks of text
+      bodyMedium: TextStyle(color: Colors.deepPurpleAccent),
+      bodySmall: TextStyle(color: Colors.deepPurpleAccent),
+
+      labelLarge: TextStyle(color: Colors.deepPurpleAccent), //Text inside of components
+      labelMedium: TextStyle(color: Colors.deepPurpleAccent),
+      labelSmall: TextStyle(color: Colors.deepPurpleAccent), 
+    //Universal Styles - Color 
+      primaryColor: Colors.blue, //Primary Application Elements
+      primaryContainer: Colors.lightBlue, 
+      secondaryColor: Colors.red,
+      secondaryContainer: Colors.orange, 
+      canvasColor: Colors.white;
+      
+      accentColor: Colors.red; //Foreground Color
+      accentColorBrightness: Brightness.light,
+
+      bottomAppBarColor: Colors.blue;
+      cardColor: Colors.blue;
+      dividerColor: Colors.blue;
+
+       unselectedWidgetColor: Colors.blue; // Inactive but visible widgets.
+       disabledColor: Colors.blue; //Disabled Widgets
+
+    //Universal Styles - UI Elements 
+    buttonColor: Colors.blue;
+    errorColor: Colors.blue; //Input Validation Input Boxes
+    /*Calendar Specifc
+    style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  fixedSize: const Size(200, 50),
+                                ),
+
+                                style: GoogleFonts.notoSerif(
+                    fontSize: 31,
+                    color: Colors.black,
+                  ),
+    */
+    /*Camera Specific
+  style: const ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStatePropertyAll(
+                                                      Colors.blue)),
+                                          child: const Text(
+                                            'Accept All',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 20),
+                                            textAlign: TextAlign.center,
+                                          ),
+
+                                          Elevated Button
+    */
+
+    //Homepage Specific - Widget has hard-coded styles, will need to convert it over.
+    
+    /*Login Specific
+'Email',
+          style: GoogleFonts.quicksand(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+
+        Elevated Button
+        */
+    //Pantry Specific - Widget has hard-coded styles, will need to convert it over.
+
+    //Profile Specific
+  /*child: Card(
+                            color: myBlue,
+                            child: SizedBox(
+                                height: 70,
+                                child: Center(
+                                    // sized box contains row of icon and text
+                                    child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Icon(Icons.lock_outline),
+                                    Text(' Login Info',
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.black)),
+                                  ], */
+
+    //Register Specific - Fonts, elevated button, containers
+
+    //Statistics Specific - Title text style
+
+    
+    
+    
+    
+    
+  
+  );
 }
+
